b1d00579a572eae2979320fbb9d292c918a93ae5
sonalika sahoo <sonalikasahoo6@gmail.com>
2018-09-23 19:41:04 +0530

    sheldon greeter

--- file02.js
+++ file02.js
@@ -1,1 +1,8 @@
 console.log("Hey! We're learning git.")
+function greet(name) {
+    console.log('knock knock knock ' + name)
+    console.log('knock knock knock ' + name)
+    console.log('knock knock knock ' + name)
+}
+
+greet('sonalika')
