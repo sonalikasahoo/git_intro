3b58762db79317928a359edd13582fa820b7a712
sonalika sahoo <sonalikasahoo6@gmail.com>
2018-09-23 20:00:02 +0530

    extra changes

--- file02.js
+++ file02.js
@@ -1,5 +1,4 @@
 console.log("Hey! We're learning git.")
-<<<<<<< HEAD
 function greet(name) {
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
@@ -7,7 +6,3 @@ function greet(name) {
 }
 
 greet('sonalika')
-=======
-console.log('Use "get status" to checkk status')
-console.log('Use "git log" to see history')
->>>>>>> gitlearning
